b1430f0164ae071e0c579fb3cfcc392455f71bbc
Anderson Rocha <anderson.rocha@addesistemas.com.br>
2021-09-11 12:12:30 -0300

    Add snake food to scene

--- src/app/pixel/pixel.component.ts
+++ src/app/pixel/pixel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pixel',
@@ -9,6 +9,7 @@ export class PixelComponent implements OnInit {
   @Input() on = false;
 
   constructor() { }
+
 
   ngOnInit(): void {
   }
--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -14,10 +14,16 @@ export class SceneComponent implements OnInit {
   onOffControllerSnapshots: Array<Array<Array<boolean>>> = [];
   isPaused = false;
   snakePosition = [
-     { i: 0, j: 0 },
-     { i: 0, j: 1 },
-     { i: 0, j: 2 },
+    { i: 0, j: 0 },
+    { i: 0, j: 1 },
+    { i: 0, j: 2 },
+    { i: 0, j: 3 },
   ];
+
+  snakeFood = [{
+    i: 5,
+    j: 15,
+  }];
 
   moveDirection = Direction.Forward;
   speed = SceneSettings.initialSpeed;
@@ -26,6 +32,7 @@ export class SceneComponent implements OnInit {
     console.log(this.snakePosition);
     this.resetOnOffController();
     this.paintSnakeFromPosition();
+    this.paintSnakeFood();
   }
 
   ngOnInit(): void {
@@ -47,10 +54,20 @@ export class SceneComponent implements OnInit {
       this.onOffController[position.i][position.j] = true;
     }
 
-    this.takeOnOffControllerSnapshot();
+    // this.takeOnOffControllerSnapshot();
   }
 
-  takeOnOffControllerSnapshot() {
+  paintSnakeFood() {
+    for (const food of this.snakeFood) {
+      if (this.snakeFood.length > 0) {
+        this.onOffController[food.i][food.j] = true;
+      }
+    }
+
+
+  }
+
+  /*takeOnOffControllerSnapshot() {
     const onOffControllerSnapshot: Array<Array<boolean>> = [];
     for (
       let lineIndex = 0;
@@ -63,7 +80,7 @@ export class SceneComponent implements OnInit {
       }
     }
     this.onOffControllerSnapshots.push(onOffControllerSnapshot);
-  }
+  }*/
 
   pause() {
     this.isPaused = !this.isPaused;
@@ -71,18 +88,25 @@ export class SceneComponent implements OnInit {
 
   @HostListener('window:keydown', ['$event'])
   keyEvent(event: KeyboardEvent) {
-    if (this.isPaused && event.key !== " ") {
+    if (this.isPaused && event.key !== ' ') {
       return;
     }
 
-
-    if (event.key === "ArrowDown") {
+    if (event.key === 'ArrowDown') {
       this.changeDirection(Direction.Down);
-    } else if (event.key === "ArrowRight") {
+    } else if (event.key === 'ArrowRight') {
       this.changeDirection(Direction.Forward);
-    } else if (event.key === " ") {
+    } else if (event.key === 'ArrowLeft') {
+      this.changeDirection(Direction.Backward);
+    } else if (event.key === 'ArrowUp') {
+      this.changeDirection(Direction.Up);
+    } else if (event.key === ' ') {
       this.pause();
     }
+  }
+
+  changeUp() {
+    this.changeDirection(Direction.Up);
   }
 
   changeDown() {
@@ -91,6 +115,10 @@ export class SceneComponent implements OnInit {
 
   changeForward() {
     this.changeDirection(Direction.Forward);
+  }
+
+  changeBackward() {
+    this.changeDirection(Direction.Backward);
   }
 
   changeDirection(direction: Direction) {
@@ -104,9 +132,13 @@ export class SceneComponent implements OnInit {
           this.moveForward();
         } else if (this.moveDirection === Direction.Down) {
           this.moveDown();
+        } else if (this.moveDirection === Direction.Backward) {
+          this.moveBackward();
+        } else if (this.moveDirection === Direction.Up) {
+          this.moveUp();
         }
-        ;
-        this.takeOnOffControllerSnapshot();
+
+        // this.takeOnOffControllerSnapshot();
       }
       this.move();
     }, this.speed);
@@ -121,26 +153,33 @@ export class SceneComponent implements OnInit {
     for (let k = 0; k < otherParts.length; k++) {
       nextPosition.push({
         i: this.snakePosition[k + 1].i,
-        j: this.snakePosition[k + 1].j
+        j: this.snakePosition[k + 1].j,
       });
     }
 
+    const newI = head!.i;
+    const newJ = head!.j == SceneSettings.columns - 1 ? 0 : head!.j + 1;
+
+    if (this.snakeFood.length > 0 && newI == this.snakeFood[0].i && newJ == this.snakeFood[0].j) {
+      console.log('Passou pela comida');
+    }
+
     nextPosition.push({
-      i: head!.i,
-      j: head!.j == SceneSettings.columns - 1 ? 0 : head!.j + 1,
+      i: newI,
+      j: newJ,
     });
 
     this.snakePosition = nextPosition;
 
-    console.log(this.snakePosition);
+    // console.log(this.snakePosition);
 
     this.resetOnOffController();
     this.paintSnakeFromPosition();
+    this.paintSnakeFood();
   }
 
-  moveDown() {
+  moveBackward() {
     const nextPosition: Array<{ i: number; j: number }> = [];
-
 
     const head = this.snakePosition.slice(-1).pop();
     const otherParts = this.snakePosition.slice(0, -1);
@@ -148,25 +187,104 @@ export class SceneComponent implements OnInit {
     for (let k = 0; k < otherParts.length; k++) {
       nextPosition.push({
         i: this.snakePosition[k + 1].i,
-        j: this.snakePosition[k + 1].j
-      })
+        j: this.snakePosition[k + 1].j,
+      });
     }
 
+    const newI = head!.i;
+    const newJ = head!.j == 0 ? SceneSettings.columns - 1 : head!.j - 1;
+
+
+
     nextPosition.push({
-      i: head!.i == SceneSettings.lines - 1 ? 0 : head!.i + 1,
-      j: head!.j
+      i: newI,
+      j: newJ,
     });
 
+    if (this.snakeFood.length > 0 && newI == this.snakeFood[0].i && newJ == this.snakeFood[0].j) {
+      console.log('Passou pela comida');
+      nextPosition.push({
+        i: this.snakeFood[0].i,
+        j: this.snakeFood[0].j == 0 ? SceneSettings.columns - 1 : this.snakeFood[0].j - 1
+      });
+      this.snakeFood = [];
+    }
+
     this.snakePosition = nextPosition;
+
     console.log(this.snakePosition);
 
     this.resetOnOffController();
     this.paintSnakeFromPosition();
+    this.paintSnakeFood();
   }
 
+  moveDown() {
+    const nextPosition: Array<{ i: number; j: number }> = [];
 
+    const head = this.snakePosition.slice(-1).pop();
+    const otherParts = this.snakePosition.slice(0, -1);
+
+    for (let k = 0; k < otherParts.length; k++) {
+      nextPosition.push({
+        i: this.snakePosition[k + 1].i,
+        j: this.snakePosition[k + 1].j,
+      });
+    }
+
+    const newI = head!.i == SceneSettings.lines - 1 ? 0 : head!.i + 1;
+    const newJ = head!.j;
+
+    if (this.snakeFood.length > 0 && newI == this.snakeFood[0].i && newJ == this.snakeFood[0].j) {
+      console.log('Passou pela comida');
+    }
+
+    nextPosition.push({
+      i: newI,
+      j: newJ,
+    });
+
+    this.snakePosition = nextPosition;
+    // console.log(this.snakePosition);
+
+    this.resetOnOffController();
+    this.paintSnakeFromPosition();
+    this.paintSnakeFood();
+  }
+
+  moveUp() {
+    const nextPosition: Array<{ i: number; j: number }> = [];
+
+    const head = this.snakePosition.slice(-1).pop();
+    const otherParts = this.snakePosition.slice(0, -1);
+
+    for (let k = 0; k < otherParts.length; k++) {
+      nextPosition.push({
+        i: this.snakePosition[k + 1].i,
+        j: this.snakePosition[k + 1].j,
+      });
+    }
+
+    const newI = head!.i == 0 ? SceneSettings.lines - 1 : head!.i - 1;
+    const newJ = head!.j;
+
+    if (this.snakeFood.length > 0 && newI == this.snakeFood[0].i && newJ == this.snakeFood[0].j) {
+      console.log('Passou pela comida');
+    }
+
+    nextPosition.push({
+      i: newI,
+      j: newJ,
+    });
+
+    this.snakePosition = nextPosition;
+    // console.log(this.snakePosition);
+
+    this.resetOnOffController();
+    this.paintSnakeFromPosition();
+    this.paintSnakeFood();
+  }
 }
-
 
 enum Direction {
   Up,
